e3f0dd02d0d45e01383a11ed98f086e747489b7b
jycurien <curien.jeanyves@gmail.com>
2023-04-17 14:52:14 +0200

    creating mole array

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react'
+
 const Game = () => {
-  return <div>TODO</div>
+  const [gameOver, setGameOver] = useState(true)
+  const [moles, setMoles] = useState([])
+
+  const generateMoles = () => {
+    const newMoles = []
+    for (let i = 0; i < 9; i++) {
+      newMoles.push({ id: i, active: false })
+    }
+    setMoles(newMoles)
+  }
+
+  const startGame = () => {
+    setGameOver(false)
+    generateMoles()
+  }
+
+  if (gameOver) {
+    return <button onClick={startGame}>Start</button>
+  }
+
+  return (
+    <div
+      className='mole-container'
+      style={{
+        gridTemplateColumns: `repeat(3, 1fr)`,
+        gridTemplateRows: `repeat(3, 1fr)`,
+      }}
+    >
+      {moles.map((mole, index) => (
+        <div key={mole.id} className={`mole ${mole.active ? 'active' : ''}`}>
+          <div>🐹</div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Game
